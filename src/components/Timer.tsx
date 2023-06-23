7b9558307ed53a21e3bdfc30101b5d61b28a3695
Johnny <jkn002@ucsd.edu>
2023-06-22 21:39:28 -0700

    Updated timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -3,29 +3,36 @@ import { LuAlarmClock } from "react-icons/lu"
 
 
 function Timer() {
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    
+    const [timerActive, setTimerActive] = useState(false);
+    const [currentTime, setCurrentTime] = useState(Date.now());
 
-    function parseTime(time: Date) {
-
-        let hours = time.getHours();
-        let minutes = time.getMinutes();
-        let seconds = time.getSeconds();
-
-        return {
-            hours, minutes, seconds
-        };
+    function startTimer() {
+        setCurrentTime(0);
+        setTimerActive(true);
     }
 
-    const [currentTime, setCurrentTime] = useState(0);
-
+    let timer : ReturnType<typeof setInterval> = 0;
+    
     useEffect( () => {
-        setTimeout( () => {
-            setCurrentTime( () => currentTime );
-        }, 500);
+        if (timerActive) {
+            timer = setInterval( () => {
+                setCurrentTime( currentTime + 1 );
+                console.log("tick");
+            }, 500);
+        }
+
+        return () => {
+            clearInterval(timer);
+        }
     }, [currentTime])
 
     return (
         <>
-            <button type="button" className={"btn btn-outline-dark"}>
+            <button type="button" className={"btn btn-outline-dark"} onClick={ startTimer }>
                 <div style={{display: "flex"}} align-content="flex-end">
                     <div style={{margin: "0px 5px", position: "relative", top: "-2px"}}>
                         <LuAlarmClock size={20}/>
@@ -36,9 +43,6 @@ function Timer() {
                 </div>
             </button>
         </>
-        
-
-        
     )
 }
 
