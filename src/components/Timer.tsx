a9d9f61d3e41d5cb1da3c51a60acd7083e79ee1d
Johnny <jkn002@ucsd.edu>
2023-07-03 18:00:30 -0700

    okfrthistime

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,49 +1,69 @@
-import { useState, useEffect } from "react";
+import { useState,useEffect } from "react";
 import { LuAlarmClock } from "react-icons/lu"
 
-function formatTime(date : Date) {
+const expiration = 3600000;
 
-    return date.getMinutes() + ":" + date.getSeconds();
+function formatTime(date : Date) {
+    let time = "";
+    if (date.getMinutes() < 10) {
+        time = "0" + date.getMinutes().toString();
+    }
+    else {
+        time = date.getMinutes().toString();
+    }
+
+    time += ":";
+
+    if (date.getSeconds() < 10) {
+        time += "0" + date.getSeconds().toString();
+    }
+    else {
+        time += date.getSeconds().toString();
+    }
+
+    return time;
 }
 
 function Timer() {
 
-    const [timerActive, setTimerActive] = useState(false);
+    const [timerIsActive, setTimerActive] = useState(false);
     const [currentTime, setCurrentTime] = useState(0);
-    //const [timeStart, setTimeStart] = useState(Date.now());
-
-    let timeStart = Date.now();
-
     const [timer, setTimer] = useState<ReturnType<typeof setInterval> | 0>(0);
-
 
     const startTimer = () => {
         setTimerActive(true);
         
-        timeStart = Date.now();
+        let timeStart = Date.now();
+        console.log("Set time start: " + new Date(timeStart).toLocaleTimeString());
+        
         setCurrentTime( Date.now() - timeStart );
-
         let interval = setInterval( () => {
             setCurrentTime( Date.now() - timeStart );
-        }, 250);
+        }, 100);
 
         setTimer(interval);
     }
 
-    function resetTimer() {
+    const resetTimer = () => {
         setTimerActive(false);
-
         clearInterval(timer);
     }
 
-    function toggleTimer() {
-        if (timerActive) {
+    const toggleTimer = () => {
+        if (timerIsActive) {
             resetTimer();
         }
         else {
             startTimer();
         }
     }
+
+    useEffect(() => {
+        if (currentTime > expiration) {
+            console.log("Timer expired at: " + Date.now().toLocaleString());
+            resetTimer();
+        }
+    }, [currentTime]);
 
     return (
         <>
@@ -53,8 +73,8 @@ function Timer() {
                         <LuAlarmClock size={20}/>
                     </div>
                     <div style={{margin: "0px 5px"}}>
-                        {timerActive && formatTime(new Date(currentTime))}
-                        {!timerActive && "--:--"}
+                        {timerIsActive && formatTime(new Date(currentTime))}
+                        {!timerIsActive && "--:--"}
                     </div>
                 </div>
             </button>
